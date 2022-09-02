b9691515604717cc9b11d6db8c720c2806bf7fff
juani-castore <juanicastore.ftw@gmail.com>
2022-09-02 01:52:14 -0300

    subo el proyecto terminado (creo)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Listado from "./components/Listado";
+import Cabecera from "./components/Cabecera";
+
+
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
 function App() {
+  
+  const [contador, setContador] = useState(0);
+
+  function sumarContador(){
+    setContador(contador+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera contador={contador} />
+      <Listado sumar={sumarContador}/>
     </div>
   );
 }
